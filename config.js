e81b2a9e2f885670fa606b2f3f7441becabccf31
Boubourazi <45666000+Boubourazi@users.noreply.github.com>
2020-10-13 12:00:34 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key:'INSERT_CONSUMER_KEY_HERE',
-	consumer_secret:'INSERT_SECRET_CONSUMER_HERE',
-	access_token:'INSERT_ACCESS_TOKEN_HERE',
-	access_token_secret:'INSERT_SECRET_ACCESS_HERE'
-}
